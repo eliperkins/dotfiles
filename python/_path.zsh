bccba02d91e9d7b5ce242406d279ef77c25546a3
Eli Perkins <eli@clubhouse.io>
2018-01-30 10:25:44 -0500

    Pin to Python 2 so npm doesn't choke

--- python/_path.zsh
+++ python/_path.zsh
@@ -2,6 +2,8 @@ export PATH="$HOME/.pyenv/bin:$HOME/Library/Python/2.7/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+export PYTHON="/usr/local/bin/python2"
+
 # pip zsh completion start
 function _pip_completion {
   local words cword
