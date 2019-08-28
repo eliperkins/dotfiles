36923ae820f7d0e2773429665fed14fae06e58ba
Eli Perkins <eli.j.perkins@gmail.com>
2019-08-28 10:41:18 -0400

    Remove pyenv

--- python/_path.zsh
+++ python/_path.zsh
@@ -1,9 +1,3 @@
-export PATH="$HOME/.pyenv/bin:$HOME/Library/Python/2.7/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-export PYTHON="/usr/local/bin/python2"
-
 # pip zsh completion start
 function _pip_completion {
   local words cword
