a323cb2d9ae4adf763f0d73f77a23340183b7507
Eli Perkins <eli.j.perkins@gmail.com>
2021-06-24 10:34:24 -0400

    Polish codespaces install script

--- install.sh
+++ install.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
+# Used for Codespaces
+
 set -euo pipefail
 
 SCRIPT_NAME=${1:-"install.sh"}
-# Send all our -x output to this file for later debugging
-LOG_DIR="$HOME/install.sh.logs"
-mkdir -p "${LOG_DIR}"
-exec 1>"${LOG_DIR}/stdout"
-exec 2>"${LOG_DIR}/stderr"
-exec 19>"${LOG_DIR}/trace"
-BASH_XTRACEFD=19
-
-set -x
 
 echo "${SCRIPT_NAME} start: $(date)"
-
-chsh -s /bin/zsh
 
 git pull
 bash script/bootstrap
