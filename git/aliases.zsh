d260d02e0910cd39a40d95cc90e33296dd40815e
Eli Perkins <eli.j.perkins@gmail.com>
2021-06-24 10:33:40 -0400

    Fix git ignore command

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,8 @@ then
   alias git=$hub_path
 fi
 
+function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
+
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
