b4de2535070c3c232ef4a9779a615d82d7168746
Eli Perkins <eli.j.perkins@gmail.com>
2021-06-28 16:48:17 -0400

    Only install Homebrew on macOS

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,7 +6,7 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew)
+if test "$(uname)" = "Darwin" && test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" > /tmp/homebrew-install.log
