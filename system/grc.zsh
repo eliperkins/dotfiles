cfec098fb498430ddca48251c6f67a6366995691
Eli Perkins <eli.j.perkins@gmail.com>
2021-06-28 16:58:37 -0400

    sick

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,5 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] )) && if test "$(uname -s)" = "Darwin"
+if (( $+commands[grc] )) && (( $+commands[brew] )) && test "$(uname -s)" = "Darwin"
 then
   source `brew --prefix`/etc/grc.bashrc
 fi
