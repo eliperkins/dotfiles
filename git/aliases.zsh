9894e4b3a64d79cdb907e779d5373a1501e32d2e
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-22 17:04:01 -0400

    Remove unused things

--- git/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
-function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
