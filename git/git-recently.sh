8c130e9d1f693cbe2fe78815b4142ed19c096722
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-22 17:04:38 -0400

    Update git aliases

---
+++ git/git-recently.sh
@@ -0,0 +1,7 @@
+recently() {
+  local branch
+  branch=$(git --no-pager branch --sort=-committerdate --format='%(refname:short) (%(color:green)%(creatordate:relative)%(color:reset))' | fzf --reverse --no-hscroll --no-multi -n 1 --ansi)
+  git switch "$(echo "$branch" | awk '{print $1}' | sed "s/.* //")"
+}
+
+recently
