2e49cb6128ccb2bd51174ea12c0c6ec1783ffb4d
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-22 18:52:46 -0400

    Clean up install scripts

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d "$HOME/.asdf" ]
 then
+    echo "  Installing asdf for you."
     git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf"
     cd "$HOME/.asdf" || exit 2
     git checkout "$(git describe --abbrev=0 --tags)"
