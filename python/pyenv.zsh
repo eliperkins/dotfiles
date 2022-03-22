7b149ad2f168c69b1bdb0d6454d29d4a07297e7c
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-22 19:39:16 -0400

    Removing unused stuff

--- python/pyenv.zsh
+++
@@ -1,6 +0,0 @@
-if which pyenv >/dev/null; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-fi
--- system/grc.zsh
+++
@@ -1,5 +0,0 @@
-# GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] )) && test "$(uname -s)" = "Darwin"
-then
-  source `brew --prefix`/etc/grc.bashrc
-fi
