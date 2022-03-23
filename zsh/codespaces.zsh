d77b6c2bd036d11a8b05750bc8f04395bd11ba42
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-23 13:16:38 -0400

    Move codespace-specific config

---
+++ zsh/codespaces.zsh
@@ -0,0 +1,8 @@
+# Codespaces-specific zsh config
+
+if [[ $CODESPACES == "true" ]]; then
+  # Configure Linuxbrew if installed
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  eval "$(brew --prefix)/bin/brew shellenv)"
+fi
