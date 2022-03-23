aeb9ab2ebfdbbc6b35c158ec296b181b5410f6f9
Eli Perkins <eli.j.perkins@gmail.com>
2022-03-23 13:20:37 -0400

    Fix typo

--- zsh/codespaces.zsh
+++ zsh/codespaces.zsh
@@ -4,5 +4,5 @@ if [[ $CODESPACES == "true" ]]; then
   # Configure Linuxbrew if installed
   test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
   test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  eval "$(brew --prefix)/bin/brew shellenv)"
+  eval "$(brew --prefix)/bin/brew shellenv"
 fi
