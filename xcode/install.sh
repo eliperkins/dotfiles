181bb13a01c8795788ceba298b7f3419ed70d389
Eli Perkins <eli.j.perkins@gmail.com>
2022-05-16 15:49:16 -0400

    Use full path for Xcode install

--- xcode/install.sh
+++ xcode/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+echo "  Installing Xcode themes for you."
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
-cp -a Themes/. ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+cp -a "$HOME/.dotfiles/xcode/Themes/." ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
