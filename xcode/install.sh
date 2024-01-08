ac53d12811cb7de682e7e96c533697266ab0d7f1
Eli Perkins <eli.j.perkins@gmail.com>
2024-01-08 23:39:44 +0000

    lol

--- xcode/install.sh
+++ xcode/install.sh
@@ -4,4 +4,4 @@ if [ "$(uname -s)" == "Darwin" ]; then
   echo "  Installing Xcode themes for you."
   mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
   cp -a "$HOME/.dotfiles/xcode/Themes/." ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-end
+fi
