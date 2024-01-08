66b4f91b28dca933447a09cb03b1e60382cceed8
Eli Perkins <eli.j.perkins@gmail.com>
2024-01-08 23:59:52 +0000

    Fix syntax error

--- xcode/install.sh
+++ xcode/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$(uname -s)" == "Darwin" ]; then
+if test "$(uname -s)" = "Darwin"; then
   echo "  Installing Xcode themes for you."
   mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
   cp -a "$HOME/.dotfiles/xcode/Themes/." ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
