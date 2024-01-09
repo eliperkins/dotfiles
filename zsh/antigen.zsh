62cbfcf2703e3755925c6c74750b6b7359cb3380
Eli Perkins <eli.j.perkins@gmail.com>
2024-01-09 00:37:11 +0000

    Remove things

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -5,8 +5,6 @@ antigen bundles <<EOBUNDLES
     zsh-users/zsh-completions
     zsh-users/zsh-history-substring-search
     zsh-users/zsh-syntax-highlighting
-    mafredri/zsh-async
-    seletskiy/zsh-fuzzy-search-and-edit
 EOBUNDLES
 
 antigen apply
