ce27baef3688a35f53e7decd1bde7f2ce6d8aaa7
Eli Perkins <eli.j.perkins@gmail.com>
2024-06-13 13:24:52 -0600

    Add jq zsh plugin

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -6,6 +6,7 @@ antigen bundles <<EOBUNDLES
     zsh-users/zsh-completions
     zsh-users/zsh-history-substring-search
     zsh-users/zsh-syntax-highlighting
+    reegnz/jq-zsh-plugin
 EOBUNDLES
 
 antigen apply
