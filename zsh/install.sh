d0586d8c836bb0ba7d562e9c679b55ea9827f44d
Eli Perkins <eli.j.perkins@gmail.com>
2024-08-27 10:20:19 -0600

    Fix Antidote install location

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,5 @@
 if [ ! -d "$HOME/.zsh/antidote" ]
 then
     echo "  Installing Antidote for you."
-    git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
+    git clone --depth=1 https://github.com/mattmc3/antidote.git $HOME/.zsh/antidote
 fi
