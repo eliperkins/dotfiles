9a3aebc466b4afd0698805dc6a9635a5170c10a8
Eli Perkins <eli.j.perkins@gmail.com>
2025-10-07 15:32:04 -0600

    Use symlink

--- vim/install.sh
+++ vim/install.sh
@@ -2,7 +2,7 @@
 
 # link all files ending in ".symlink" in this "vim" directory (and it's subfolders) to the "$HOME/.vim" directory, maintaining folder structure
 # for example, link "./vim/autoload/plug.vim.symlink" to "$HOME/.vim/autoload/plug.vim"
-for file in $(find vim -type f -name "*.symlink"); do
+for file in $(find -L vim -type f -name "*.symlink"); do
     dir=$(dirname "$file")
     name=$(basename "$file" .symlink)
     mkdir -p "$HOME/.$dir"
